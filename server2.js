f387e48544b89085c271ea94c18739f47ee34fcf
Max Ockner <maxockner@gmail.com>
2023-07-22 18:21:06 -0700

    change port

--- server2.js
+++ server2.js
@@ -34,5 +34,5 @@ app.on('error', (err, ctx) => {
     console.error('Server error', err);
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(5000, () => console.log('Server is running on port 5000'));
 
